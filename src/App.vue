07d08930ad72a095555aa147349a2db5a4c3eaf4
Jamal Hasuev <jhasuev@mail.ru>
2023-02-17 15:44:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <FmRadio />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import FmRadio from './components/FmRadio.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    FmRadio,
+  },
+});
+</script>
---
+++ src/components/FmRadio.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <audio ref="audioPlayer" controls>
+      <track kind="captions" />
+    </audio>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
